81b79a6c578700edaddb6c07366ea9fa74630132
Luis Rivera <luisantonioriveraalvarado@gmail.com>
2020-10-07 12:57:04 -0500

    First commit, installing dependencies, defining main and recycler item view´s

---
+++ app/build/generated/ap_generated_sources/debug/out/com/lara/mvvmmtvshows/BR.java
@@ -0,0 +1,7 @@
+package com.lara.mvvmmtvshows;
+
+public class BR {
+  public static final int _all = 0;
+
+  public static final int isLoading = 1;
+}
---
+++ app/build/generated/source/dataBinding/trigger/debug/com/lara/mvvmmtvshows/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package com.lara.mvvmmtvshows;
+
+import androidx.databinding.BindingBuildInfo;
+
+@BindingBuildInfo
+public class DataBindingInfo {}
